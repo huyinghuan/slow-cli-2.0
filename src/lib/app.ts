ccdf34c2e8635433ff754af8090946b6d9f9438a
huyinghuan <ec.huyinghuan@gmail.com>
2016-10-14 01:30:54 +0800

    init

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,8 +1,21 @@
 import * as _express from 'express';
 import * as _http from 'http';
+import * as _colors from 'colors';
+const _app = _express();
 
-const _app = _express()
+
+_app.use('/', (request, response) => {
+
+})
+
 
 const _server = _http.createServer(_app)
+
+_server.on('error', (error) => {
+  console.log(error)
+})
+
+
+
 
 _server.listen(_app.listen(3000));
