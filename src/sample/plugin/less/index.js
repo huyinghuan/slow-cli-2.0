de0c34bfb7298c6cc124052b962ebd3418b51108
huyinghuan <ec.huyinghuan@gmail.com>
2016-11-01 09:57:15 +0800

    add less

---
+++ src/sample/plugin/less/index.js
@@ -0,0 +1,59 @@
+'use strict';
+const _url = require('url');
+const _path = require('path');
+const _fs = require('fs');
+const _less = require('less');
+const _ = require('lodash');
+
+
+var _DefaultSetting = {
+  "regexp": "(\.css|\.less)$",
+  "options":{}
+}
+
+//判断该文件是否需要处理
+const isNeedCompile = (pathname)=>{
+  let reg = new RegExp(_DefaultSetting.regexp)
+  return reg.test(pathname)
+}
+
+//根据实际路径获取文件内容
+const getCompileContent = (realFilePath, data, cb)=>{
+  if(!_fs.existsSync(realFilePath)){
+    data.status = 404
+    return cb(null, data, null)
+  }
+
+  let fileContent = _fs.readFileSync(realFilePath, {encoding: 'utf8'})
+  
+  try{
+    
+    //编译成功，标记状态码
+    data.status = 200;
+    //这里可以添加数据获取逻辑 TODO
+    cb(null, data, template({}))
+  }catch(e){
+    cb(e)
+  }
+} 
+
+exports.registerPlugin = function(cli, options){
+  //继承定义
+  _.extend(_DefaultSetting, options.setting);
+  
+  cli.registerHook('route:didRequest', (req, data, content, cb)=>{
+    //如果不需要编译
+    if(!isNeedCompile(req.path)){
+      return cb(null, data, content)
+    }  
+    let fakeFilePath = _path.join(process.cwd(), _DefaultSetting.root, req.path);
+    //替换路径为hbs
+    let realFilePath = fakeFilePath.replace(/(html)$/,'hbs')
+
+    getCompileContent(realFilePath, data, (error, data, content)=>{
+      if(error){return cb(error)};
+      //交给下一个处理器
+      cb(null, data, content)
+    })
+  })
+}
