70f8bb09d9a490ba9d21a58c416ed65020ae3319
hyh <ec.huyinghuan@gmail.com>
2016-11-16 18:36:42 +0800

    rm  redundancy file

--- hooks/getFullPluginName.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//获取插件完成名称
-function default_1(pluginName) {
-    return pluginName;
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
--- init/formatBuildConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function default_1() {
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
