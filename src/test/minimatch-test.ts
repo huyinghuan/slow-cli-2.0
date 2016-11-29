d75fc58e43ef1f58ae816431d0f464bad4773029
hyh <ec.huyinghuan@gmail.com>
2016-11-29 15:04:20 +0800

    :fire: remove test file

--- src/test/minimatch-test.ts
+++
@@ -1,9 +0,0 @@
-import * as _minimatch from 'minimatch';
-import * as _assert from 'assert';
-import * as _should from 'should';
-
-describe("minimatch test", ()=>{
-  it("/ax/axx/xx.html *.html true", ()=>{
-    _should(_minimatch("/ax/axx/xx.html", "*.+(html|hbs)", {matchBase: true})).be.true();
-  })
-})
--- test/minimatch-test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const _minimatch = require('minimatch');
-const _should = require('should');
-describe("minimatch test", () => {
-    it("/ax/axx/xx.html *.html true", () => {
-        _should(_minimatch("/ax/axx/xx.html", "*.+(html|hbs)", { matchBase: true })).be.true();
-    });
-});
