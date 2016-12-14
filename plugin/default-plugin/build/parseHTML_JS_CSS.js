5c35d700e0610d8ae689aec8146cc29816014bc9
hyh <ec.huyinghuan@gmail.com>
2016-12-14 18:12:17 +0800

    去掉测试注释

--- plugin/default-plugin/build/parseHTML_JS_CSS.js
+++ plugin/default-plugin/build/parseHTML_JS_CSS.js
@@ -1,13 +1,13 @@
 "use strict";
+const _fs = require('fs');
 exports.registerPlugin = function (cli, options) {
-    //   cli.registerHook('build:doCompile', (buildConfig, data, content, cb)=>{
-    //     console.log(buildConfig, data, content)
-    //     let inputFilePath = data.inputFilePath;
-    //     if(!/((\.html)|(\.js)|(\.css))$/.test(inputFilePath)){
-    //       return cb(null, content)
-    //     }
-    //     _fs.readFile(inputFilePath, 'utf8', (err, result)=>{
-    //       cb(err, result)
-    //     });
-    // }, 0)
+    cli.registerHook('build:doCompile', (buildConfig, data, content, cb) => {
+        let inputFilePath = data.inputFilePath;
+        if (!/((\.html)|(\.js)|(\.css))$/.test(inputFilePath)) {
+            return cb(null, content);
+        }
+        _fs.readFile(inputFilePath, 'utf8', (err, result) => {
+            cb(err, result);
+        });
+    }, 0);
 };
--- src/plugin/default-plugin/build/parseHTML_JS_CSS.ts
+++ src/plugin/default-plugin/build/parseHTML_JS_CSS.ts
@@ -1,13 +1,12 @@
 import * as _fs from 'fs'
 exports.registerPlugin = function(cli, options){
-  //   cli.registerHook('build:doCompile', (buildConfig, data, content, cb)=>{
-  //     console.log(buildConfig, data, content)
-  //     let inputFilePath = data.inputFilePath;
-  //     if(!/((\.html)|(\.js)|(\.css))$/.test(inputFilePath)){
-  //       return cb(null, content)
-  //     }
-  //     _fs.readFile(inputFilePath, 'utf8', (err, result)=>{
-  //       cb(err, result)
-  //     });
-  // }, 0)
+    cli.registerHook('build:doCompile', (buildConfig, data, content, cb)=>{
+      let inputFilePath = data.inputFilePath;
+      if(!/((\.html)|(\.js)|(\.css))$/.test(inputFilePath)){
+        return cb(null, content)
+      }
+      _fs.readFile(inputFilePath, 'utf8', (err, result)=>{
+        cb(err, result)
+      });
+  }, 0)
 }
