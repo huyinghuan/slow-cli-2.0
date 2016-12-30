f232ceea8b21d3eb54ed0c2495cd0a99b2c5e46a
hyh <ec.huyinghuan@gmail.com>
2016-12-30 15:07:39 +0800

    :fire: remove files

--- .silky/production/global.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const _path = require("path")
-const projectName = process.cwd().split(_path.sep).pop();
-module.exports = {
-  __img: `//img.hunantv.com/${projectName}/`,
-  __css: `//css.hunantv.com/${projectName}/`,
-  __js: `//js.hunantv.com/${projectName}/`,
-}
