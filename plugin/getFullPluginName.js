227135722afd971567ae5900ec852572264eb076
hyh <ec.huyinghuan@gmail.com>
2017-01-10 18:13:10 +0800

    :fire: remove plugin prefix

--- plugin/getFullPluginName.js
+++ plugin/getFullPluginName.js
@@ -4,9 +4,6 @@ function default_1(pluginName, needVersion) {
     let pluginStrArray = pluginName.split('@');
     pluginName = pluginStrArray[0];
     let version = pluginStrArray[1];
-    if (!/^(sp\-)/.test(pluginName)) {
-        pluginName = `sp-${pluginName}`;
-    }
     return (needVersion && version) ? `${pluginName}@${version}` : pluginName;
 }
 Object.defineProperty(exports, "__esModule", { value: true });
--- src/plugin/getFullPluginName.ts
+++ src/plugin/getFullPluginName.ts
@@ -3,10 +3,5 @@ export default function(pluginName, needVersion?){
   let pluginStrArray = pluginName.split('@')
   pluginName = pluginStrArray[0];
   let version = pluginStrArray[1];
-
-  if(!/^(sp\-)/.test(pluginName)){
-    pluginName = `sp-${pluginName}`
-  }
-
   return (needVersion && version)  ? `${pluginName}@${version}`: pluginName;
 }
