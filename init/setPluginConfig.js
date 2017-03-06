016f9c6d75f16d1f03f616b33ecad0bf928e22ec
hyh <ec.huyinghuan@gmail.com>
2017-03-06 17:54:29 +0800

    :fire: 删除多余文件

--- init/setPluginConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const file_config_1 = require('../file-config');
-const _ = require('lodash');
-function default_1(config, pluginConfig) {
-    config[file_config_1.default.pluginConfigField] = _.extend(config[file_config_1.default.pluginConfigField], pluginConfig);
-    return config;
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
