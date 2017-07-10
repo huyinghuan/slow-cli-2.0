01fc8802dc54984e32a4e90b2022af5c9a599807
hyh <ec.huyinghuan@gmail.com>
2017-07-10 15:16:25 +0800

    :fire: remove code

--- src/lib/getGitHash.ts
+++ src/lib/getGitHash.ts
@@ -3,6 +3,5 @@ import _configFiledConstant from '../config-filed-constant';
 
 export default function(cb){
   let workspace = _configFiledConstant.getWorkspace()
-  let commandStr = `git log -1 --format="%H"`;
   _gitHeadHash(workspace, cb)
 }
